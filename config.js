d6bc1154ad3b66b46301f6e6b37613698d3c85fc
abituru <105980644+abituru@users.noreply.github.com>
2022-06-17 19:16:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -17,11 +17,11 @@ second: 'numeric'
 function pickRandom(list) {
   return list[Math.floor(Math.random() * list.length)]
 }
-global.owner = ['17608914335'] // Put your number here
+global.owner = ['6288232154376'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
-  // name: 'https://website'
+  // name: 'Ｍａｒｉｎ－ＭＤ'
   amel: 'https://melcanz.com',
     bx: 'https://bx-hunter.herokuapp.com',
   dhnjing: 'https://dhnjing.xyz',
